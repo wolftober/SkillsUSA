003cdf1e2a4455e7e3242f81dfb290d51355d7bb
bvaic <vaiciulis.ben@gmail.com>
2023-02-11 23:55:06 -0600

    bullet despawn script

---
+++ Skills Game/Assets/Scripts/bulletDespawn.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletDespawn : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+
+}
