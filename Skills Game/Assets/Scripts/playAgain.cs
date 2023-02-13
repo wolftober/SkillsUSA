0e0d7e10f3adc55e19c45a62ee36218adee68044
bvaic <vaiciulis.ben@gmail.com>
2023-02-12 22:09:48 -0600

    more endgame stuff

--- Skills Game/Assets/Scripts/playAgain.cs
+++ Skills Game/Assets/Scripts/playAgain.cs
@@ -2,16 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class playAgain : MonoBehaviour
 {
     public Button button;
+    public GameObject player;
+    public TMP_Text gameOverText;
+    public GameObject playAgainButton;
+    public TMP_Text timeText;
 
     void Start()
     {
         Button b = button.GetComponent<Button>();
-        //b.onClick.AddListener(OnClick);
+        b.onClick.AddListener(OnClick);
     }
 
+    void OnClick()
+    {
+        player.SetActive(true);
+        gameOverText.text = "";
+        playAgainButton.SetActive(false);
 
+        // restart the clock
+        timeText.text = "0";
+        timeText.GetComponent<Score>().currentTime = 0;
+        timeText.GetComponent<Score>().track = true;
+
+        // ========= Start Spawning ==========
+
+    }
 }
