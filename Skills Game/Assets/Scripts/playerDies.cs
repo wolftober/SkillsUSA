519cf05c1885e973ddccc05f28ee5745a32c4d4d
bvaic <vaiciulis.ben@gmail.com>
2023-03-26 17:05:23 -0500

    started health bar

--- Skills Game/Assets/Scripts/playerDies.cs
+++ Skills Game/Assets/Scripts/playerDies.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class playerDies : MonoBehaviour
@@ -9,40 +10,47 @@ public class playerDies : MonoBehaviour
     public TMP_Text gameoverText;
     public GameObject playAgain;
     public GameObject returnToMenu;
+    public GameObject healthbar;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            gameObject.SetActive(false);
+            // damage player
+            healthbar.GetComponent<Slider>().value -= 10;
 
-            // game over / reset
-
-            // ========== Stop Spawning ==========
-
-
-            // deleting all enemies
-
-            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
-            foreach(GameObject enemy in enemies)
+            if (healthbar.GetComponent<Slider>().value <= 0)
             {
-                GameObject.Destroy(enemy);
+                gameObject.SetActive(false);
+
+                // game over / reset
+
+                // ========== Stop Spawning ==========
+
+
+                // deleting all enemies
+
+                GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+                foreach (GameObject enemy in enemies)
+                {
+                    GameObject.Destroy(enemy);
+                }
+
+                // stopping time and getting the end score
+                timeText.GetComponent<Score>().track = false;
+
+                int finalScore = (int)timeText.GetComponent<Score>().currentTime;
+
+                // game over text
+                gameoverText.text = "Game Over! \n You stayed alive for " + finalScore + " seconds.";
+
+                // turning on the play again button
+                playAgain.SetActive(true);
+
+                // turning on the return to menu button
+                returnToMenu.SetActive(true);
+                healthbar.GetComponent<Slider>().value = 100;
             }
-
-            // stopping time and getting the end score
-            timeText.GetComponent<Score>().track = false;
-            
-            int finalScore = (int) timeText.GetComponent<Score>().currentTime;
-
-            // game over text
-            gameoverText.text = "Game Over! \n You stayed alive for " + finalScore + " seconds.";
-
-            // turning on the play again button
-            playAgain.SetActive(true);
-
-            // turning on the return to menu button
-            returnToMenu.SetActive(true);
-
         }
     }
 
