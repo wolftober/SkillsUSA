ef3d396244b5ff44b5cbc79f90bbff6d6a36edc6
wolftober <84695508+wolftober@users.noreply.github.com>
2023-03-26 22:58:17 -0500

    fixed goofus

--- Skills Game/Assets/Scripts/PickUpController.cs
+++ Skills Game/Assets/Scripts/PickUpController.cs
@@ -7,55 +7,70 @@ public class PickUpController : MonoBehaviour
    public Shooting gunScript;
    public Rigidbody rb;
    public BoxCollider coll;
-   public Transform player, gunContainer;
+   public Transform gunContainer;
+   private Transform player;
+   private GunManager pistol;
    
    public float pickUpRange;
    public float dropForwardForce, dropUpwardForce;
-   public int ammo;
+   public float despawnTime;
+   //public int ammo;
 
    public bool equipped;
+   private bool used;
    public static bool slotFull;
 
    private void Start(){
-        ammo = gunScript.GetComponent<Shooting>().current_ammo;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
         //setup
         if(!equipped){
             gunScript.enabled = false;
             rb.isKinematic = false;
             coll.isTrigger = false;
+            used = false;
         }
         if(equipped){
             gunScript.enabled = true;
             rb.isKinematic = true;
             coll.isTrigger = true;
             slotFull = true;
+            used = true;
         }
    }
 
     private void Update(){
+
         //Check if player is in range and "E" is pressed
         Vector3 distanceTopPlayer = player.position - transform.position;
-        if(!equipped && distanceTopPlayer.magnitude <= pickUpRange && Input.GetKeyDown(KeyCode.E) && !slotFull) PickUp();
+        if(!equipped && distanceTopPlayer.magnitude <= pickUpRange && !slotFull && !used) PickUp();
 
         //Drop if ammo = 0
-        if(equipped && ammo == 0) Drop();
+        if(equipped && gunScript.current_ammo == 0) Drop();
     }
    private void PickUp(){
         equipped = true;
         slotFull = true;
+        used = true;
+
+        //make RigidBody kinematic and boxcollider a trigger
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        rb.isKinematic = true;
+        coll.isTrigger = true;
 
         //Make weapon a child of the character and move it to default position
         transform.SetParent(gunContainer);
         transform.localPosition = Vector3.zero;
         transform.localRotation = Quaternion.Euler(Vector3.zero);
-        transform.localScale = Vector3.one;
-
-        //make RigidBody kinematic and boxcollider a trigger
-        rb.isKinematic = true;
-        coll.isTrigger = true;
+        //transform.localScale = Vector3.one;
 
         //enable Script
         gunScript.enabled = true;
+        gunScript.gameObject.GetComponent<Collider>().enabled = false;
+
+        //pistol
+        player.GetComponent<GunManager>().SetPistolActive(false);
    }
 
    private void Drop(){
@@ -81,5 +96,16 @@ public class PickUpController : MonoBehaviour
 
         //disable Script
         gunScript.enabled = false;
+        gunScript.gameObject.GetComponent<Collider>().enabled = true;
+
+        //pistol
+        player.GetComponent<GunManager>().SetPistolActive(true);
+
+        StartCoroutine(Despawn());
+   }
+
+   private IEnumerator Despawn(){
+        yield return new WaitForSeconds(despawnTime);
+        Destroy(this.gameObject);
    }
 }
