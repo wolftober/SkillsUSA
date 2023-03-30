d3277ed35bf5adb881bf9f7bf112d0745e0e210a
wolftober <84695508+wolftober@users.noreply.github.com>
2023-03-30 02:33:19 -0500

    00

--- Skills Game/Assets/Scripts/WASD.cs
+++ Skills Game/Assets/Scripts/WASD.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class WASD : MonoBehaviour
 {
     public float speed = 0.02f;
+    public Transform spawn;
 
     // Update is called once per frame
     void Update()
@@ -16,5 +17,11 @@ public class WASD : MonoBehaviour
         Vector3 moveDirection = new Vector3(xDirec, 0.0f, zDirec); // creates the move force
 
         transform.position += moveDirection * speed;
+    }
+
+    private void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.CompareTag("wall")){
+            this.gameObject.transform.position = spawn.transform.position;
+        }
     }
 }
