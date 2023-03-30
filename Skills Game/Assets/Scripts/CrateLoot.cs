66b0336d7d4f8e807431f1f456fbb595ced6f1d0
wolftober <84695508+wolftober@users.noreply.github.com>
2023-03-30 00:49:36 -0500

    fixed crate

--- Skills Game/Assets/Scripts/CrateLoot.cs
+++ Skills Game/Assets/Scripts/CrateLoot.cs
@@ -8,8 +8,9 @@ public class CrateLoot : MonoBehaviour
     public Transform box;
 
     private void OnCollisionEnter(Collision collision){
-        if(collision.gameObject.CompareTag("Bullet"))
+        if(collision.gameObject.CompareTag("Bullet")){
             Instantiate(drop, box.position,  Quaternion.identity);
             Destroy(gameObject);
+        }
     }
 }
