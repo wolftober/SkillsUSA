569ebdce3b802ebd1dd0f2b230ee9148e469fe7a
wolftober <84695508+wolftober@users.noreply.github.com>
2023-03-30 00:18:43 -0500

    item drop script

---
+++ Skills Game/Assets/Scripts/CrateLoot.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateLoot : MonoBehaviour
+{
+    public GameObject drop;
+    public Transform box;
+
+    private void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.CompareTag("Bullet"))
+            Instantiate(drop, box.position,  Quaternion.identity);
+            Destroy(gameObject);
+    }
+}
